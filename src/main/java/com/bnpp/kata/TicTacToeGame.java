39dd837dec6f87339945b45c2207be0e0874cdfe
2020-DEV-007 <2020DEV007@gmail.com>
2019-11-26 00:26:41 +0530

    Added test case to check if initial player is X

--- src/main/java/com/bnpp/kata/TicTacToeGame.java
+++ src/main/java/com/bnpp/kata/TicTacToeGame.java
@@ -2,4 +2,8 @@ package com.bnpp.kata;
 
 class TicTacToeGame {
 
+	String getCurrentPlayer() {
+		return "X";
+	}
+
 }
--- src/test/java/com/bnpp/kata/TicTacToeGameTest.java
+++ src/test/java/com/bnpp/kata/TicTacToeGameTest.java
@@ -1,5 +1,6 @@
 package com.bnpp.kata;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
@@ -9,5 +10,10 @@ public class TicTacToeGameTest {
 	@Test
 	public void newGameInstanceIsNotNull() {
 		assertNotNull(new TicTacToeGame());
+	}
+
+	@Test
+	public void testInitialPlayerMustBeX() {
+		assertEquals("X", new TicTacToeGame().getCurrentPlayer());
 	}
 }
